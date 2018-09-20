b1bf7573207ec637d55e8878cb19547a1d66c517
leonardo <leomagari.os@gmail.com>
2018-09-19 22:47:38 -0300

    hago pseudoCodigo y creo una funcion en ficha para comprobar si esta clickeada

--- tp2/entregable/js/ficha.js
+++ tp2/entregable/js/ficha.js
@@ -1,16 +1,28 @@
 class Ficha{
-    constructor(){
+    constructor(posx,posy,radio,skin){
         this.propietarioName="";
-        this.skinId="";
-        this.posx=0;
-        this.posy=0;
-        this.width=50;
-        this.height=50;
-        this.radio=50;
+        this.skinId=null;
+        this.posx=posx;
+        this.posy=posy;
+        this.width=radio*2;
+        this.height=radio*2;
+        this.radio=25;
         this.dragged=false;
     }
     draw(ctx){
         //dibujar ficha en el canvas
+        ctx.beginPath();
+        ctx.arc(this.posx,this.posy,this.radio,0,2*Math.PI);
+        ctx.fillStyle="#00c";
+        ctx.lineCap="rounded";
+        ctx.strokeStyle = "#00f";
+        ctx.lineWidth = 5;
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+    isClicked(pos){
+        return (Math.pow(pos.x-this.posx,2)+Math.pow(pos.y-this.posy,2) < Math.pow(this.radio,2));
     }
     drag(){
         this.dragged=true;
--- tp2/entregable/js/main.js
+++ tp2/entregable/js/main.js
@@ -5,3 +5,51 @@ function drawUi(ctx){
     //dibujar rectangulo contenedor de datos del juego
     //NombreJ1,NombreJ2,TurnoActual,PiezasRestantes.
 }
+//FUNCIONES
+function getMousePos(canvas,evt){
+    var rect= canvas.getBoundingClientRect();
+    return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+    };
+}
+
+//let nombreJugador1="";
+//let nombreJugador2="";
+let btnStart=document.getElementById("btnStart");
+btnStart.onclick=(ev){
+    ev.preventDefault();
+    nombreJugador1=document.getElementById("jugador1").innerText;
+    nombreJugador2=document.getElementById("jugador2").val;
+}
+
+//TEST
+function testDrawFicha(ficha,ctx){
+    ficha.draw(ctx);
+}
+
+function testIsClickedFicha(ficha,pos){
+    console.log(ficha.isClicked(pos));
+}
+
+//test dibujo ficha
+let ficha=new Ficha(50,50,50,"hola");
+let pos=null;
+canvas.onmousemove=(evt)=>{
+    pos=getMousePos(canvas, evt);
+    //moverFicha si fichaSeleccionada!==null
+}
+document.onmouseup=()=>{
+    //fichaSeleccionada.drop();
+    //fichaSeleccionada=null;
+    //check si Ficha drop esta en ranuraTablero
+}
+canvas.onclick=()=>{
+    //fichaSeleccionada = fichaClicked
+    //ficha ficha.drag();
+   
+    console.log(ficha.isClicked(pos));
+}
+testDrawFicha(ficha,ctx);
+
+//test click ficha
